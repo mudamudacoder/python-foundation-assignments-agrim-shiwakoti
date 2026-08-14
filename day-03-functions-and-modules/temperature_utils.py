795cef20bf2e04b420165e5ec01d36f59efc3919
mudamudacoder <agrimsiwakoti5@gmail.com>
2026-08-14 12:29:28 +0545

    day 3 ipynb and py module added

---
+++ day-03-functions-and-modules/temperature_utils.py
@@ -0,0 +1,14 @@
+'''
+Exercise 5: Temperature Utilities
+Student: Agrim Shiwakoti
+Day: 3
+'''
+
+MODULE_VERSION = "1.0"
+
+# temperature conversion functions
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
